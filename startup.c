afca6f3f884fc45a71c01eb5b4053d52091e98a9
Anders Westrup <anders@stickybit.se>
2014-05-27 22:03:55 +0200

    Initialize variables

--- startup.c
+++ startup.c
@@ -1,7 +1,18 @@
 #define STACK_SIZE 1024
 unsigned int stack[STACK_SIZE];
 
+extern unsigned int flash_start_data, ram_start_data, ram_end_data, start_bss, end_bss;
+
+void reset_handler() __attribute__ ((naked));
 void reset_handler() {
+  unsigned int *flash_data = &flash_start_data, *ram_data = &ram_start_data, *bss = &start_bss;
+  for (unsigned int i=0; i < (&ram_end_data - &ram_start_data); i++) {
+    ram_data[i] = flash_data[i];
+  }
+  for (unsigned int i=0; i < (&end_bss - &start_bss); i++) {
+    bss[i] = 0;
+  }
+
   while (1);
 }
 
